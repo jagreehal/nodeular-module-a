d61b0e8226596c4bb98bd97e23bf12059e8a2b0b
Jag Reehal <jag.reehal@gmail.com>
2016-03-07 20:11:21 +0000

    Now passes in app

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var app = require('nodeular-core/express');
 var api = require('./api');
 
-app.get('/a', function (req, res){
-  setTimeout(function (){
-    var date = res.locals.dateStamp || new Date();
-    return res.status(200).send('a - ' + date);
-  }, 1000);
-});
 
-app.use('/a/api', api);
+function init(app) {
 
-module.exports = app;
+  app.get('/a', function (req, res) {
+    setTimeout(function () {
+      var date = res.locals.dateStamp || new Date();
+      return res.status(200).send('a - ' + date);
+    }, 1000);
+  });
+
+  app.use('/a/api', api);
+}
+
+module.exports = init;
